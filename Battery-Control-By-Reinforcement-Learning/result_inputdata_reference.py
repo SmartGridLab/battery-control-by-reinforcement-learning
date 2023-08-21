c53e15c4b921b53888f592b044f3e375f4c8d850
Takuya510634 <ef510634.takuya@gmail.com>
2023-08-21 09:48:40 +0000

    evaluation.pyを実装する #52

--- Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py
+++ Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py
@@ -23,19 +23,20 @@ def main():
     energyprice_actual = energyprice_actual.values.flatten()
     imbalanceprice_actual = imbalanceprice_actual.values.flatten()
 
-    # データが格納されている最後の行番号を取得(何行目からデータを挿入するか判定するため)
+
+    ### 実績データの挿入
+    # 実績データが格納されている最後の行番号を取得(何行目からデータを挿入するか判定するため)
     last_data_row = dataframe['PV_actual'].last_valid_index()
     i = last_data_row
-    # 0行目から格納するとき(まだデータがないとき)
+    # 0行目から格納するとき(1日目のデータを入れる場合はdataframeにまだデータがない)
     if i == None:
         i = -1
-
     # i+1行目からi+48行目に実績データを挿入
     dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('PV_actual')] = PV_actual
     dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('energyprice_actual')] = energyprice_actual
     dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('imbalanceprice_actual')] = imbalanceprice_actual
    
-    # 結果を上書き保存
+    # result_dataframe.csvを上書き保存
     dataframe.to_csv("Battery-Control-By-Reinforcement-Learning/result_dataframe.csv", index=False)
 
     print("-実績データ書き込み完了-")
