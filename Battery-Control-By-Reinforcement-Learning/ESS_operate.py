fbcd26722da64e2ffd4a24c8b53f4859fc9af815
Takuya510634 <ef510634.takuya@gmail.com>
2023-10-23 08:29:37 +0000

    ESS_operated.pyを実装する #51

--- Battery-Control-By-Reinforcement-Learning/ESS_operate.py
+++ Battery-Control-By-Reinforcement-Learning/ESS_operate.py
@@ -11,6 +11,9 @@ class ESS_operate:
         # result_dataframe.csvを読み込む
         dataframe = pd.read_csv("Battery-Control-By-Reinforcement-Learning/result_dataframe.csv")
 
+        # 蓄電池容量
+        battery_MAX = 4
+
         # 指定した(制御する)日時の部分だけ抽出する
         operate_dataframe = dataframe[(dataframe['year'] == year) &
                   (dataframe['month'] == month) &
@@ -22,34 +25,45 @@ class ESS_operate:
 
         # PVが計画よりも多い場合
         if delta_PV >= 0:
-            # if:蓄電池動作なし
-                # 充電量増加(売電量変化なし)
-                
-                # if:SoCが100に到達
+            # 蓄電池動作なし、充電中の場合
+            if operate_dataframe['charge/discharge_bid'] <= 0:
+                # 充電量増加、売電量変化なし
+                operate_dataframe['charge/discharge_actual'] - delta_PV #充電量は負の値
+                operate_dataframe['energytransfer_actual'] = operate_dataframe['energytransfer_bid']
+                #operate_dataframe['SoC_actual'] = operate_dataframe['SoC_bid'] + (delta_PV * 0.5)/battery_MAX   #出力[kW]を30分あたりの電力量[kWh]に変換、定格容量[kWh]で割って[%]変換
+            
+                # SoCが100に到達した場合
+                if operate_dataframe['SoC_actual'] > 100:
+                    soc_over_enegy = (operate_dataframe['SoC_actual'] - 100) * battery_MAX / 0.5    #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
                     # 充電量はSoC100までの量
+                    operate_dataframe['charge/discharge_actual'] + soc_over_enegy #充電量は負の値のため、正方向が減少
+                    operate_dataframe['SoC_actual'] = 100
                     # のこりは売電量に回す
+                    operate_dataframe['energytransfer_actual'] += soc_over_enegy
 
-            # if:充電時
-                # 充電量増加(売電量変化なし)
-                
-                # if:SoCが100に到達
-                    # 充電量はSoC100までの量
-                    # のこりは売電量に回す
 
-            # if:放電時
-                # if:delta_PV<放電出力
-                    #放電量抑制(売電量変化なし)
+            # 放電時の場合
+            else :
+                # delta_PV<放電出力の場合
+                if delta_PV < operate_dataframe['energytransfer_bid']:
+                    #放電量抑制、売電量変化なし
+                    operate_dataframe['energytransfer_actual'] = operate_dataframe['energytransfer_bid']
+                    operate_dataframe['charge/discharge_actual'] -= delta_PV
+                    operate_dataframe['SoC_actual'] = operate_dataframe['SoC_bid'] - (delta_PV * 0.5)/battery_MAX
+
                 # if:delta_PV>放電出力
-                    # 充電になる(売電量変化なし)
+                else:
+                    # 充電に切り替え、売電量変化なし
+                    operate_dataframe['energytransfer_actual'] = operate_dataframe['energytransfer_bid']
+                    operate_dataframe['charge/discharge_actual'] -= delta_PV
+                    operate_dataframe['SoC_actual'] = operate_dataframe['SoC_bid'] - (delta_PV * 0.5)/battery_MAX
 
                     # if:SoCが100に到達
                         # 充電量はSoC100までの量
                         # のこりは売電量に回す
-
-            operate_dataframe['energytransfer_bid'] += delta_PV
-
         # PVが計画よりも多い場合
         else :
+            print("エラー除けのダミー")
             # if:蓄電池動作なし
                 # 放電量増加(売電量変化なし)
                 
@@ -74,7 +88,6 @@ class ESS_operate:
                 # if:SoCが0に到達
                     # 放電量はSoC0までの量
                     # 差分だけ売電量減少
-            operate_dataframe['energytransfer_bid'] -= delta_PV
 
 
 
