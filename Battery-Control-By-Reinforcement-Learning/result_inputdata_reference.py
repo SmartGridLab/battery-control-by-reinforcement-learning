2371ef25aa2786c4f8492c7806bbd17588aaeef1
Takuya510634 <ef510634.takuya@gmail.com>
2023-11-02 05:28:57 +0000

    #50  #51 #52 微調整・一括動作用プログラム

--- Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py
+++ Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py
@@ -33,8 +33,8 @@ def main():
         i = -1
     # i+1行目からi+48行目に実績データを挿入
     dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('PV_actual')] = PV_actual
-    dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('energyprice_actual')] = energyprice_actual
-    dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('imbalanceprice_actual')] = imbalanceprice_actual
+    dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('energyprice_actual')] = energyprice_actual * 0.5
+    dataframe.iloc[i+1:i+49, dataframe.columns.get_loc('imbalanceprice_actual')] = imbalanceprice_actual * 0.5
    
     # result_dataframe.csvを上書き保存
     dataframe.to_csv("Battery-Control-By-Reinforcement-Learning/result_dataframe.csv", index=False)
--- Battery-Control-By-Reinforcement-Learning/test_move.py
+++ Battery-Control-By-Reinforcement-Learning/test_move.py
@@ -2,6 +2,10 @@
 import os
 import subprocess
 
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/weather_data_bid.py'])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/pv_predict.py'])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/price_predict.py'])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_schedule_dev.py'])
 subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_dataframe_manager.py'])
 subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_writing_bid.py'])
 subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py'])
