7fac79e35c883547cc2dc21cd8accee7dcc40ca1
Takuya510634 <ef510634.takuya@gmail.com>
2023-11-13 08:27:04 +0000

    realtime結果入力

--- Battery-Control-By-Reinforcement-Learning/test_move.py
+++ Battery-Control-By-Reinforcement-Learning/test_move.py
@@ -2,12 +2,13 @@
 import os
 import subprocess
 
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/weather_data_bid.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/pv_predict.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/price_predict.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_schedule_dev.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/weather_data_bid.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/pv_predict.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/price_predict.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_schedule_dev.py'])
 subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_dataframe_manager.py'])
 subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_writing_bid.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_operate.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_evaluration.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_writing_realtime.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_operate.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_evaluration.py'])
