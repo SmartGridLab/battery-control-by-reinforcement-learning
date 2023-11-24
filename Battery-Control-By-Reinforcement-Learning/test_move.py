dd466f6bacb00b74afec34bf65dfcc1c5ff50b58
Takuya510634 <ef510634.takuya@gmail.com>
2023-11-24 02:37:42 +0000

    テストプログラム

--- Battery-Control-By-Reinforcement-Learning/test_move.py
+++ Battery-Control-By-Reinforcement-Learning/test_move.py
@@ -1,14 +1,25 @@
-#一括動作用
+#一括動作(テスト)
 import os
 import subprocess
 
-#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/weather_data_bid.py'])
-#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/pv_predict.py'])
-#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/price_predict.py'])
+#送るデータ
+data_to_send = {
+    'year': 2022,
+    'month': 4,
+    'day': 12,
+    'hour':23.5
+}
+
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/weather_data_bid.py', str(data_to_send)])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/weather_data_realtime.py', str(data_to_send)])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/pv_predict.py'])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/price_predict.py'])
 #subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_schedule_dev.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_dataframe_manager.py'])
-subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_writing_bid.py'])
-#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_writing_realtime.py'])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_realtime_dev.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_dataframe_manager.py'])
+#subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_writing_bid.py'])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_writing_plan.py'])
 #subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_inputdata_reference.py'])
 #subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_operate.py'])
+subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/ESS_operate_realtime.py'])
 #subprocess.run(['python', 'Battery-Control-By-Reinforcement-Learning/result_evaluration.py'])
