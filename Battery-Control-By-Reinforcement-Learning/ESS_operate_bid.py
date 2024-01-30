3928f94853d92f5c7975521a744222c9112c264a
daisukekodaira <daisuke.kodaira03@gmail.com>
2024-01-30 07:18:45 +0000

    mofify ESS_operat

--- Battery-Control-By-Reinforcement-Learning/ESS_operate_bid.py
+++ Battery-Control-By-Reinforcement-Learning/ESS_operate_bid.py
@@ -50,13 +50,12 @@ def battery_operation(self):
             if soc < 0:
                 # オーバーした出力
                 soc_over_enegy = (0-soc)*0.01*self.BATTERY_CAPACITY / 0.5 #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
-                # 放電量はSoC0までの量
+                # 放電量はSoCが0[%]になるまでの量
                 self.df_result.at[j, 'charge/discharge_actual_bid'] -= soc_over_enegy
                 soc = 0
                 # 差分だけ売電量減少
                 self.df_result.at[j, 'energytransfer_actual_bid'] -= soc_over_enegy
-
-                self.df_result.at[j, 'mode'] = 5
+                self.df_result.at[j, 'mode'] = 5 # Case5:
                     
 
         # PVが計画よりも少ない場合
@@ -65,19 +64,20 @@ def battery_operation(self):
             self.df_result.at[j, 'mode'] = -1
 
             # 充電量抑制(放電量増加)・売電量変化なし
-            self.df_result.at[j, 'charge/discharge_actual_bid'] = self.df_result.at[j, 'charge/discharge_bid'] + abs(delta_PV)    #充電量は負の値なので、値を正の方向へ
+            self.df_result.at[j, 'charge/discharge_actual_bid'] = self.df_result.at[j, 'charge/discharge_bid'] + abs(self.delta_PV)    #充電量は負の値なので、値を正の方向へ
             self.df_result.at[j, 'energytransfer_actual_bid'] = self.df_result.at[j, 'energytransfer_bid']
-
         
             # SoCの計算
             if j == 0:
-                previous_soc = 0 ###マジックナンバー
+                previous_soc = self.INITIAL_SOC  # この実装で良いかは要検討
             else:
                 previous_soc = self.df_result.at[j, 'SoC_actual_bid']
             soc = previous_soc - (self.df_result.at[j, 'charge/discharge_actual_bid']*0.5)*100/self.BATTERY_CAPACITY
 
             # SoCが100に到達した場合
             if soc > 100:
+                # caseを記録
+                self.df_result.at[j, 'mode'] = -3
                 # オーバーした入力
                 soc_over_enegy = (soc-100)*0.01*self.BATTERY_CAPACITY / 0.5    #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
                 # 充電量はSoC100までの量
@@ -86,10 +86,10 @@ def battery_operation(self):
                 # 差分は売電量を増加させる
                 self.df_result.at[j, 'energytransfer_actual_bid'] += soc_over_enegy
 
-                self.df_result.at[j, 'mode'] = -3
-
             # if:SoCが0に到達
             if soc < 0:
+                # caseを記録
+                self.df_result.at[j, 'mode'] = -5
                 # オーバーした出力
                 soc_over_enegy = (0-soc)*0.01*self.BATTERY_CAPACITY / 0.5 #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
                 # 放電量はSoC0までの量
@@ -98,23 +98,12 @@ def battery_operation(self):
                 # 差分だけ売電量減少
                 self.df_result.at[j, 'energytransfer_actual_bid'] -= soc_over_enegy
 
-                self.df_result.at[j, 'mode'] = -5
-
         # energytransfer_actual_bidの修正
         if self.df_result.at[j, 'energytransfer_actual_bid'] < 0:
             self.df_result.at[j, 'energytransfer_actual_bid'] = 0
+            ## デバッグ用。energytransfer_actual_bidが負の値になっていたらおかしいので-999を入れておく
+            self.df_result.at[j, 'mode'] = -999
 
-            self.df_result.at[j, 'mode'] *= 2
-
-        # SoCの最終処理
-        if j == 0:
-            self.df_result.at[j, 'SoC_actual_bid'] = previous_soc
-        if j < last_csv_row:
-            self.df_result.at[j+1, 'SoC_actual_bid'] = soc  #最終行以降にsoc格納するスペースはない
-        
-        # サイクル
-        i += 1
-    ###
 
     # result_self.df_result.csvを上書き保存
     self.df_result.to_csv("Battery-Control-By-Reinforcement-Learning/result_self.df_result.csv", index=False)
