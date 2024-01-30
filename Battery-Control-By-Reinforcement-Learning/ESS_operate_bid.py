19a04cec81578f0be75ba97500b4c45677d8485d
daisukekodaira <daisuke.kodaira03@gmail.com>
2024-01-30 06:10:52 +0000

    operate_bid修正中

--- Battery-Control-By-Reinforcement-Learning/ESS_operate_bid.py
+++ Battery-Control-By-Reinforcement-Learning/ESS_operate_bid.py
@@ -1,115 +1,122 @@
 import pandas as pd
+import parameters as Param
 
-# 蓄電池容量
-battery_MAX = 4
 
-# result_dataframe.csvを読み込む
-df_result = pd.read_csv("Battery-Control-By-Reinforcement-Learning/result_dataframe.csv")
+def __init__(self):
+    # パラメータクラスのインスタンス化
+    param = Param() 
+    # パラメータの読み込み
+    self.BATTERY_CAPACITY = param.BATTERY_CAPACITY
+    self.INITIAL_SOC = param.INITIAL_SOC
 
-print("-bidモード機器動作開始-")
+    # RLの結果を書き込んだファイル(result_self.df_result.csv)を読み込む
+    self.df_result = pd.read_csv("Battery-Control-By-Reinforcement-Learning/result_self.df_result.csv")
 
-# PVの予測値('PV_actual')と実測値('PV_predict_bid')の差を計算
-delta_PV = df_result["PV_actual"] - df_result["PV_predict_bid"]
+    # PVの予測値('PV_actual')と実測値('PV_predict_bid')の差を計算
+    self.delta_PV = self.df_result["PV_actual"] - self.df_result["PV_predict_bid"]
 
-for i in range(len(df_result):
+def battery_operation(self):
+    for j in range(len(self.df_result)):
+        # PVが計画よりも多い場合
+        if self.delta_PV >= 0:
+            # caseを記録
+            self.df_result.at[j, 'operation_case'] = 1 #Case1: 充電量増加(放電量抑制)・売電量変化なし
 
-    # PVが計画よりも多い場合
-    if delta_PV >= 0:
-        # 充電量増加(放電量抑制)・売電量変化なし
-        dataframe.at[j, 'charge/discharge_actual_bid'] = dataframe.at[j, 'charge/discharge_bid'] - abs(delta_PV) #充電量は負の値なので、値を負の方向へ
-        dataframe.at[j, 'energytransfer_actual_bid'] = dataframe.at[j, 'energytransfer_bid']
-
-        dataframe.at[j, 'mode'] = 1
-    
-        ## SoCのチェック
-        # SoCの計算
-        if j == 0:
-            before_soc = 0 ###マジックナンバー
-        else:
-            before_soc = dataframe.at[j, 'SoC_actual_bid']
-        soc = before_soc - (dataframe.at[j, 'charge/discharge_actual_bid']*0.5)*100/battery_MAX   #出力[kW]を30分あたりの電力量[kWh]に変換、定格容量[kWh]で割って[%]変換
+            # 充電量増加(放電量抑制)・売電量変化なし
+            self.df_result.at[j, 'charge/discharge_actual_bid'] = self.df_result.at[j, 'charge/discharge_bid'] - abs(self.delta_PV) #充電量は負の値なので、値を負の方向へ
+            self.df_result.at[j, 'energytransfer_actual_bid'] = self.df_result.at[j, 'energytransfer_bid']
         
-        # SoCが100に到達した場合
-        if soc > 100:
-            # オーバーした入力
-            soc_over_enegy = (soc-100)*0.01*battery_MAX / 0.5    #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
-            # 充電量はSoC100までの量
-            dataframe.at[j, 'charge/discharge_actual_bid'] += soc_over_enegy #充電量は負の値のため、正方向が減少
-            soc = 100
-            # 差分は売電量を増加させる
-            dataframe.at[j, 'energytransfer_actual_bid'] += soc_over_enegy
+            ## SoCのチェック
+            # SoCの計算
+            if j == 0:
+                previous_soc = self.INITIAL_SOC ### この実装で良いかは要検討
+            else:
+                previous_soc = self.df_result.at[j, 'SoC_actual_bid']
+            # 出力[kW]を30分あたりの電力量[kWh]に変換、定格容量[kWh]で割って[%]変換
+            soc = previous_soc - (self.df_result.at[j, 'charge/discharge_actual_bid']*0.5)*100/self.BATTERY_CAPACITY
+            
+            # SoCが100[%]に到達した場合
+            if soc > 100:
+                self.df_result.at[j, 'mode'] = 3 # Case3: 
+                # 充電できないPV発電量の計算
+                soc_over_enegy = (soc-100)*0.01*self.BATTERY_CAPACITY / 0.5    #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
+                # 充電量はSoC100までの量
+                self.df_result.at[j, 'charge/discharge_actual_bid'] += soc_over_enegy #充電量は負の値のため、正方向が減少
+                soc = 100
+                # 差分は売電量を増加させる
+                self.df_result.at[j, 'energytransfer_actual_bid'] += soc_over_enegy
+            
+            # SoCが0に到達
+            if soc < 0:
+                # オーバーした出力
+                soc_over_enegy = (0-soc)*0.01*self.BATTERY_CAPACITY / 0.5 #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
+                # 放電量はSoC0までの量
+                self.df_result.at[j, 'charge/discharge_actual_bid'] -= soc_over_enegy
+                soc = 0
+                # 差分だけ売電量減少
+                self.df_result.at[j, 'energytransfer_actual_bid'] -= soc_over_enegy
 
-            dataframe.at[j, 'mode'] = 3
-        
-        # SoCが0に到達
-        if soc < 0:
-            # オーバーした出力
-            soc_over_enegy = (0-soc)*0.01*battery_MAX / 0.5 #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
-            # 放電量はSoC0までの量
-            dataframe.at[j, 'charge/discharge_actual_bid'] -= soc_over_enegy
-            soc = 0
-            # 差分だけ売電量減少
-            dataframe.at[j, 'energytransfer_actual_bid'] -= soc_over_enegy
+                self.df_result.at[j, 'mode'] = 5
+                    
 
-            dataframe.at[j, 'mode'] = 5
-                
-
-    # PVが計画よりも少ない場合
-    else:
-        # 充電量抑制(放電量増加)・売電量変化なし
-        dataframe.at[j, 'charge/discharge_actual_bid'] = dataframe.at[j, 'charge/discharge_bid'] + abs(delta_PV)    #充電量は負の値なので、値を正の方向へ
-        dataframe.at[j, 'energytransfer_actual_bid'] = dataframe.at[j, 'energytransfer_bid']
-
-        dataframe.at[j, 'mode'] = -1
-    
-        # SoCの計算
-        if j == 0:
-            before_soc = 0 ###マジックナンバー
+        # PVが計画よりも少ない場合
         else:
-            before_soc = dataframe.at[j, 'SoC_actual_bid']
-        soc = before_soc - (dataframe.at[j, 'charge/discharge_actual_bid']*0.5)*100/battery_MAX
+            # caseを記録
+            self.df_result.at[j, 'mode'] = -1
 
-        # SoCが100に到達した場合
-        if soc > 100:
-            # オーバーした入力
-            soc_over_enegy = (soc-100)*0.01*battery_MAX / 0.5    #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
-            # 充電量はSoC100までの量
-            dataframe.at[j, 'charge/discharge_actual_bid'] += soc_over_enegy #充電量は負の値のため、正方向が減少
-            soc = 100
-            # 差分は売電量を増加させる
-            dataframe.at[j, 'energytransfer_actual_bid'] += soc_over_enegy
+            # 充電量抑制(放電量増加)・売電量変化なし
+            self.df_result.at[j, 'charge/discharge_actual_bid'] = self.df_result.at[j, 'charge/discharge_bid'] + abs(delta_PV)    #充電量は負の値なので、値を正の方向へ
+            self.df_result.at[j, 'energytransfer_actual_bid'] = self.df_result.at[j, 'energytransfer_bid']
 
-            dataframe.at[j, 'mode'] = -3
+        
+            # SoCの計算
+            if j == 0:
+                previous_soc = 0 ###マジックナンバー
+            else:
+                previous_soc = self.df_result.at[j, 'SoC_actual_bid']
+            soc = previous_soc - (self.df_result.at[j, 'charge/discharge_actual_bid']*0.5)*100/self.BATTERY_CAPACITY
 
-        # if:SoCが0に到達
-        if soc < 0:
-            # オーバーした出力
-            soc_over_enegy = (0-soc)*0.01*battery_MAX / 0.5 #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
-            # 放電量はSoC0までの量
-            dataframe.at[j, 'charge/discharge_actual_bid'] -= soc_over_enegy
-            soc = 0
-            # 差分だけ売電量減少
-            dataframe.at[j, 'energytransfer_actual_bid'] -= soc_over_enegy
+            # SoCが100に到達した場合
+            if soc > 100:
+                # オーバーした入力
+                soc_over_enegy = (soc-100)*0.01*self.BATTERY_CAPACITY / 0.5    #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
+                # 充電量はSoC100までの量
+                self.df_result.at[j, 'charge/discharge_actual_bid'] += soc_over_enegy #充電量は負の値のため、正方向が減少
+                soc = 100
+                # 差分は売電量を増加させる
+                self.df_result.at[j, 'energytransfer_actual_bid'] += soc_over_enegy
 
-            dataframe.at[j, 'mode'] = -5
+                self.df_result.at[j, 'mode'] = -3
 
-    # energytransfer_actual_bidの修正
-    if dataframe.at[j, 'energytransfer_actual_bid'] < 0:
-        dataframe.at[j, 'energytransfer_actual_bid'] = 0
+            # if:SoCが0に到達
+            if soc < 0:
+                # オーバーした出力
+                soc_over_enegy = (0-soc)*0.01*self.BATTERY_CAPACITY / 0.5 #オーバーしたSoC[%] -> 30分あたりの電力量[kWh] -> 出力[kW]
+                # 放電量はSoC0までの量
+                self.df_result.at[j, 'charge/discharge_actual_bid'] -= soc_over_enegy
+                soc = 0
+                # 差分だけ売電量減少
+                self.df_result.at[j, 'energytransfer_actual_bid'] -= soc_over_enegy
 
-        dataframe.at[j, 'mode'] *= 2
+                self.df_result.at[j, 'mode'] = -5
 
-    # SoCの最終処理
-    if j == 0:
-        dataframe.at[j, 'SoC_actual_bid'] = before_soc
-    if j < last_csv_row:
-        dataframe.at[j+1, 'SoC_actual_bid'] = soc  #最終行以降にsoc格納するスペースはない
-    
-    # サイクル
-    i += 1
-###
+        # energytransfer_actual_bidの修正
+        if self.df_result.at[j, 'energytransfer_actual_bid'] < 0:
+            self.df_result.at[j, 'energytransfer_actual_bid'] = 0
 
-# result_dataframe.csvを上書き保存
-dataframe.to_csv("Battery-Control-By-Reinforcement-Learning/result_dataframe.csv", index=False)
+            self.df_result.at[j, 'mode'] *= 2
 
-print("-bidモード機器動作終了-")
+        # SoCの最終処理
+        if j == 0:
+            self.df_result.at[j, 'SoC_actual_bid'] = previous_soc
+        if j < last_csv_row:
+            self.df_result.at[j+1, 'SoC_actual_bid'] = soc  #最終行以降にsoc格納するスペースはない
+        
+        # サイクル
+        i += 1
+    ###
+
+    # result_self.df_result.csvを上書き保存
+    self.df_result.to_csv("Battery-Control-By-Reinforcement-Learning/result_self.df_result.csv", index=False)
+
+    print("-bidモード機器動作終了-")
--- Battery-Control-By-Reinforcement-Learning/parameters.py
+++ Battery-Control-By-Reinforcement-Learning/parameters.py
@@ -1,29 +1,57 @@
-# For neural network
-ALPHA_ = 0.05 #　measure of the confidence level of the prediction intervals
-SOFTEN_ = 160 # parameter of Loss to smooth the NN learning curve
-N_LAMBDA = 5 # parameter of Loss that ratios MPIW to PICP
-BETA = 0.01 # the exponential decay rate for the 1st moment estimates in Adam optimization
-EPOCHS = 1000 # number of NN learning times
-LR = 0.001 # learnning rate of NN
-SEED = 2021 # seed of random numbers
+## This file is for setting the parameters of the simulation.
+## You can change the parameters for your simulation.
+## The parameters are used in the following files:
+# PV_predict:
+#   - Battery-Control-By-Reinforcement-Learning/pv_predict.py
 
-# Simulation conditons
-DAYS = 2 # days to be forecasted    #weather_data_xxx.csvで入力されたものを導入する場合は2で固定する(1セットを1日分と扱われる)
-N_VERIFICATION = 4 # the number of verification; to examine the variability of the modeling error
+# ESS_operate_bid:
+#   - Battery-Control-By-Reinforcement-Learning/ESS_operate_bid.py
+#   - Battery-Control-By-Reinforcement-Learning/ESS_operate_realtime.py
+#   - Battery-Control-By-Reinforcement-Learning/RL_env.py
+#   - Battery-Control-By-Reinforcement-Learning/RL_main.py
+#   - Battery-Control-By-Reinforcement-Learning/RL_train.py
+#   - Battery-Control-By-Reinforcement-Learning/RL_model.py
 
-# For Quantile regression
-LOWER_ALPHA = 0.025
-UPPER_ALPHA = 0.975
-LR=0.05
-M_TR=3
-M_LE=7
-N_E=500
-SEED_QR = 0
 
-# Select the features from top. 
-# - The number of features could be from 1 to 14. 
-# - If you chose 1, only radiation is utilized for PV forcasting.
-# - (1) 'SolarIrradiation', (2) 'hourSin', (3) 'hourCos', 'yearCos', 'CloudCover',....
-#       'yearSin', 'Humidity', 'Temp', 'monthCos', 'WindSpeed', 'daySin',
-#       'dayCos', 'monthSin', (14) 'Rain'
-NUMBER_OF_FEATURES = 4
+
+from multiprocessing.pool import INIT
+
+
+class Parameters():
+    # For neural network
+    ALPHA_ = 0.05 #　measure of the confidence level of the prediction intervals
+    SOFTEN_ = 160 # parameter of Loss to smooth the NN learning curve
+    N_LAMBDA = 5 # parameter of Loss that ratios MPIW to PICP
+    BETA = 0.01 # the exponential decay rate for the 1st moment estimates in Adam optimization
+    EPOCHS = 1000 # number of NN learning times
+    LR = 0.001 # learnning rate of NN
+    SEED = 2021 # seed of random numbers
+
+    # Simulation conditons
+    DAYS = 2 # days to be forecasted    #weather_data_xxx.csvで入力されたものを導入する場合は2で固定する(1セットを1日分と扱われる)
+    N_VERIFICATION = 4 # the number of verification; to examine the variability of the modeling error
+
+    # For Quantile regression
+    LOWER_ALPHA = 0.025
+    UPPER_ALPHA = 0.975
+    LR=0.05
+    M_TR=3
+    M_LE=7
+    N_E=500
+    SEED_QR = 0
+
+    # Select the features from top. 
+    # - The number of features could be from 1 to 14. 
+    # - If you chose 1, only radiation is utilized for PV forcasting.
+    # - (1) 'SolarIrradiation', (2) 'hourSin', (3) 'hourCos', 'yearCos', 'CloudCover',....
+    #       'yearSin', 'Humidity', 'Temp', 'monthCos', 'WindSpeed', 'daySin',
+    #       'dayCos', 'monthSin', (14) 'Rain'
+    NUMBER_OF_FEATURES = 4
+
+    ## For RL
+
+
+    ## Battery specification
+    BATTERY_CAPACITY = 4 # [kWh]
+    INITIAL_SOC = 0.5 # [0,1]で設定。[0,100]に対応
+
